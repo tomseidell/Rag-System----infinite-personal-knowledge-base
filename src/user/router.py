1ff2ce492e6c1aeb9cd7453c6bc8e6e78febd846
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-10-21 13:57:19 +0200

    add user login route

--- src/user/router.py
+++ src/user/router.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, Depends
 
 from user.dependencies import get_user_service
-from user.schemas import UserRegistration, UserResponse
+from user.schemas import UserLogin, UserRegistration, UserResponse
 from user.service import UserService
 
 
@@ -10,3 +10,8 @@ router = APIRouter()
 @router.post("/register", status_code=201, response_model=UserResponse)
 async def register(user:UserRegistration, user_service:UserService = Depends(get_user_service)):
     return await user_service.create_user(user)
+
+
+@router.post("/login", status_code=200, response_model=UserResponse)
+async def login(user:UserLogin, user_service:UserService = Depends(get_user_service)):
+    return await user_service.login_user(user)
