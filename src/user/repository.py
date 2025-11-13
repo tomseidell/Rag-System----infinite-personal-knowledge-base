5463e402c67c48eaecedda0bac696f22b21a97c3
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-11-13 10:40:32 +0100

    add error handling to use repository

--- src/user/repository.py
+++ src/user/repository.py
@@ -1,7 +1,8 @@
 from datetime import datetime
-from fastapi import HTTPException
 from pydantic import EmailStr
+from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.ext.asyncio import AsyncSession
+from core.exceptions import DatabaseException
 from src.user.model import User
 from sqlalchemy import select, update  
 
@@ -11,44 +12,74 @@ class UserRepository:
         self.db = db
 
     async def create_user(self, full_name:str, email:EmailStr, hashed_password:str)-> User:
-        db_user = User(
-            full_name = full_name,
-            email = email,
-            hashed_password = hashed_password 
-        )
+        try:
+            db_user = User(
+                full_name = full_name,
+                email = email,
+                hashed_password = hashed_password 
+            )
+            self.db.add(db_user)
+            await self.db.commit()
+            await self.db.refresh(db_user)
+            return db_user
+        except SQLAlchemyError as e:
+            await self.db.rollback()
+            raise DatabaseException(
+                operation="create_user",
+                detail= str(e)
+            )
 
-        self.db.add(db_user)
-        await self.db.commit()
-        await self.db.refresh(db_user)
 
-        return db_user
+    async def get_user_by_mail(self, email:EmailStr) -> User | None:
+        try:
+            stmt = select(User).where(User.email == email)
+            result = await self.db.execute(stmt)
+            user = result.scalar_one_or_none()
+            return user
+        except SQLAlchemyError as e:
+            raise DatabaseException(
+                operation="get_user_by_mail",
+                detail = str(e)
+            )
 
 
-    async def get_user_by_mail(self, email:EmailStr) -> User:
-        stmt = select(User).where(User.email == email)
-        result = await self.db.execute(stmt)
-        user = result.scalar_one_or_none()
-
-        return user
-
-    async def get_user_by_id(self, id:int) -> User:
-        stmt = select(User).where(User.id == id)
-        result = await self.db.execute(stmt)
-        user = result.scalar_one_or_none()
-
-        return user
+    async def get_user_by_id(self, id:int) -> User | None:
+        try: 
+            stmt = select(User).where(User.id == id)
+            result = await self.db.execute(stmt)
+            user = result.scalar_one_or_none()
+            return user
+        except SQLAlchemyError as e:
+            raise DatabaseException(
+                operation="get_user_by_id",
+                detail = str(e)
+            )
 
 
     async def update_last_login(self, id:int) -> None:
-        stmt = update(User).where(User.id == id).values(
-            last_login = datetime.now()
-        )
-        await self.db.execute(stmt)
-        await self.db.commit()
+        try:
+            stmt = update(User).where(User.id == id).values(
+                last_login = datetime.now()
+            )
+            await self.db.execute(stmt)
+            await self.db.commit()
+        except SQLAlchemyError as e:
+            await self.db.rollback()
+            raise DatabaseException(
+                operation="update_last_login",
+                detail=str(e)
+            )
 
     async def update_refresh_token(self, id:int, token:str) -> None:
-        stmt = update(User).where(User.id == id).values(
-            refresh_token=token
-        )
-        await self.db.execute(stmt)
-        await self.db.commit()
+        try:
+            stmt = update(User).where(User.id == id).values(
+                refresh_token=token
+            )
+            await self.db.execute(stmt)
+            await self.db.commit()
+        except SQLAlchemyError as e:
+            await self.db.rollback()
+            raise DatabaseException(
+                operation="update_refresh_token",
+                detail = str(e)
+            ) 
