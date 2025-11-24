3d3c93942f056e7b895a9a1e41d82e39e8108d29
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-11-24 18:28:24 +0100

    exceptions (error handling)

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -2,6 +2,6 @@ class DatabaseException(Exception):
     def __init__(self, operation:str, detail:str):
         self.operation = operation
         self.detail = detail
-        self.message = f"Database error occured in: {operation}"
+        self.message = f"Database error occured in: {self.operation}"
         super().__init__(self.message)
         
