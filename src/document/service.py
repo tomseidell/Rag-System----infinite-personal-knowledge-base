fd5eedccca777a837511d6695df5f9db4635c35c
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-11-24 19:31:47 +0100

    create Document type in service

--- src/document/service.py
+++ src/document/service.py
@@ -2,6 +2,9 @@ from fastapi import HTTPException, UploadFile
 from src.document.model import Document
 from src.document.repository import DocumentRepository
 import hashlib
+from src.document.schemas import DocumentCreate
+
+from src.core.exceptions import InputError
 
 from storage import get_bucket
 
@@ -14,7 +17,10 @@ class DocumentService:
         content = await file.read()
 
         if not file.filename:
-            raise HTTPException(status_code=400, detail="Filename missing")
+            raise InputError(
+                operation="upload_document",
+                detail="Filename is missing"
+            )
         
         content_type = file.content_type or "application/octet-stream" # fallback undefined content type
         content_hash = self._calculate_hash(content=content)
@@ -29,9 +35,25 @@ class DocumentService:
 
         source_type = self._get_source_type_from_file(filename=file.filename)
         storage_path = self._upload_to_bucket(content, filename=file.filename, user_id=user_id, content_type=content_type)
-        db_document = await self.document_repository.create_document(user_id=user_id, title=title, original_filename=file.filename, storage_path = storage_path, file_size=len(content), file_type=content_type, source_type=source_type, content_hash=content_hash, chunk_count=0)
+
+        document = DocumentCreate(
+            user_id = user_id,
+            title = title,
+            original_filename = file.filename,
+            storage_path = storage_path,
+            file_size=len(content),
+            file_type=content_type,
+            source_type=source_type,
+            content_hash=content_hash,
+            source_id=None,
+            chunk_count=0
+        )
+
+        db_document = await self.document_repository.create_document(document)
 
         return db_document
+
+
 
     def _calculate_hash(self, content:bytes) ->str:
         hashed_string = hashlib.sha256(content).hexdigest()
