8e966533616fdec2559c344a0c8be8da212a793c
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-11-24 19:31:11 +0100

    add input exception

--- src/core/exceptions.py
+++ src/core/exceptions.py
@@ -5,3 +5,10 @@ class DatabaseException(Exception):
         self.message = f"Database error occured in: {self.operation}"
         super().__init__(self.message)
         
+
+class InputError(Exception):
+    def __init__(self, operation, detail):
+        self.operation = operation
+        self.detail = detail
+        self.message = f"Input validation error occured in: {self.operation}"
+        super().__init__(self.message)
