232daab90600d3551e4d10f64e5adf656e5d2a43
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-11-25 15:41:28 +0100

    inject storage service as dependency for document service when creating instance

--- src/document/dependencies.py
+++ src/document/dependencies.py
@@ -6,10 +6,13 @@ from src.database import get_db
 
 from fastapi import Depends
 
+from storage.service import StorageService
+from src.storage.dependencies import get_storage_service
+
 
 
 def get_document_repository(db: AsyncSession = Depends(get_db)) -> DocumentRepository:
      return DocumentRepository(db)
 
-def get_document_service(repo: DocumentRepository = Depends(get_document_repository)):
-    return DocumentService(repo) 
+def get_document_service(repo: DocumentRepository = Depends(get_document_repository), storage:StorageService = Depends(get_storage_service)):
+    return DocumentService(repo, storage) 
