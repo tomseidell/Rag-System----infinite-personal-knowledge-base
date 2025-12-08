8b5b19d4e48cb297439976d79791c6924227b515
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-08 17:45:27 +0100

    add user id to chunk table

--- src/chunk/model.py
+++ src/chunk/model.py
@@ -10,6 +10,7 @@ class Chunk(Base):
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
     document_id: Mapped[int] = mapped_column(Integer, ForeignKey("documents.id", ondelete="CASCADE"), index=True)
+    user_id : Mapped[int] = mapped_column(Integer, ForeignKey("users.id", ondelete="CASCADE"), index= True)
     chunk_index: Mapped[int] = mapped_column(Integer)
     text: Mapped[str] = mapped_column(Text)
     token_count: Mapped[int] = mapped_column(Integer)
