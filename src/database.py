4ba7ab11f23ebb7726460dc3e33ca4b60f4d4c50
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-11 16:28:09 +0100

    add sync db connection for celery

--- src/database.py
+++ src/database.py
@@ -1,5 +1,8 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession, async_sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
 
 from src.config import settings
 
@@ -20,9 +23,26 @@ AsyncSessionLocal = async_sessionmaker( # creates sessions which communicate wit
 
 Base = declarative_base() # parent class of all db models 
 
+sync_database_url = f"postgresql+psycopg2://{settings.DATABASE_USER}:{settings.DATABASE_PASSWORD}@{settings.DB_HOST}:{settings.DATABASE_PORT}/{settings.DB_NAME}"
+
+sync_engine = create_engine(database_url)
+SyncSessionLocal = sessionmaker(
+    bind = sync_engine,
+    autocommit=False,
+    autoflush=False,
+)
+
 async def get_db(): # creates new sessison for each request : in each incoming request this function gets called to have the db ready and 
     async with AsyncSessionLocal() as session:
         try:
             yield session # returns session and pauses function 
         finally:
             await session.close() #closes session in the end 
+
+
+def get_sync_db():
+    db = SyncSessionLocal()
+    try:
+        yield db
+    except:
+        db.close
