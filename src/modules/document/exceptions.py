14e375bab901a262959a5def06545611b947e0c7
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-15 18:55:26 +0100

    document utils exceptions

--- src/modules/document/exceptions.py
+++ src/modules/document/exceptions.py
@@ -14,3 +14,17 @@ class DocumentAlreadyExistsException(DocumentException):
         super().__init__(self.message)
 
 
+
+#utils
+class PDFProcessingException(Exception):
+    def __init__(self, detail:str):
+        self.detail = detail
+        self.message = f"PDF processing failed: {detail}"
+        super().__init__(self.message)
+
+
+class TextSplittingException(Exception):
+    def __init__(self,detail:str):
+        self.detail = detail
+        self.message= f"Splitting text failed: {detail}"
+        super().__init__(self.message)
