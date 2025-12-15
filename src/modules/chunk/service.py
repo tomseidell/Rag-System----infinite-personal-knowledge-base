aca4855612a431924697713502ad6dbe55be5218
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-15 18:55:11 +0100

    add sync chunk service

---
+++ src/modules/chunk/service.py
@@ -0,0 +1,19 @@
+from src.modules.chunk.model import Chunk
+from src.modules.chunk.repository import ChunkRepositorySync
+
+class ChunkServiceSync():
+
+    def __init__(self, repo :ChunkRepositorySync):
+        self.repo = repo
+
+    def create_chunks_from_text(self,chunks:list[str], document_id:int, user_id:int):
+        chunk_objects=[]
+        for i, chunk in enumerate(chunks, start=1): 
+            chunk_objects.append(Chunk(
+                text=chunk,
+                document_id=document_id,
+                chunk_index=i,
+                user_id=user_id  
+            ))
+
+        return self.repo.flush_many(chunks=chunk_objects)
