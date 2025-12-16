1972121a4d4897ceb175f25566d1200ba6b9569d
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-16 20:58:12 +0100

    add params

--- src/modules/document/dependencies.py
+++ src/modules/document/dependencies.py
@@ -15,10 +15,19 @@ from src.clients.qdrant.dependencies import get_qdrant_service
 
 from src.modules.user.dependencies import get_user_repository
 
+from src.modules.chunk.service import ChunkServiceAsync
+from src.modules.chunk.dependencies import get_chunk_service_async
+
 
 
 def get_document_repository(db: AsyncSession = Depends(get_db)) -> DocumentRepository:
      return DocumentRepository(db)
 
-def get_document_service(document_repo: DocumentRepository = Depends(get_document_repository), storage:StorageService = Depends(get_storage_service), user_repo:UserRepository = Depends(get_user_repository), qdrant_service:AsyncQdrantService = Depends(get_qdrant_service)):
-    return DocumentService(document_repository=document_repo, storage=storage,user_repository=user_repo, qdrant_service=qdrant_service) 
+def get_document_service(
+     document_repo: DocumentRepository = Depends(get_document_repository),
+     storage:StorageService = Depends(get_storage_service),
+     user_repo:UserRepository = Depends(get_user_repository),
+     qdrant_service:AsyncQdrantService = Depends(get_qdrant_service),
+     chunk_service:ChunkServiceAsync = Depends(get_chunk_service_async)
+     ):
+    return DocumentService(document_repository=document_repo, storage=storage,user_repository=user_repo, qdrant_service=qdrant_service, chunk_service=chunk_service) 
