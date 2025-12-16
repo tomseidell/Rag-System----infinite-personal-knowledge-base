f49b50dfd77bfc247f8369395f577c96211e6a12
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-16 20:57:54 +0100

    Async chunk service

--- src/modules/chunk/service.py
+++ src/modules/chunk/service.py
@@ -1,5 +1,7 @@
 from src.modules.chunk.model import Chunk
-from src.modules.chunk.repository import ChunkRepositorySync
+from src.modules.chunk.repository import ChunkRepositorySync, ChunkRepository
+from qdrant_client.models import ExtendedPointId
+
 
 class ChunkServiceSync():
 
@@ -18,3 +20,13 @@ class ChunkServiceSync():
             ))
 
         return self.repo.flush_many(chunks=chunk_objects)
+    
+
+class ChunkServiceAsync():
+    def __init__(self, repositoy: ChunkRepository):
+        self.repository = repositoy
+
+    async def get_chunks_for_doc(self, document_id:int, user_id:int)->list[ExtendedPointId]:
+        result = await self.repository.get_chunks_for_doc(document_id=document_id, user_id=user_id)
+        chunk_ids: list[ExtendedPointId] = [chunk.id for chunk in result]
+        return chunk_ids
