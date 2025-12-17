87d1656b4b9f041e3c04ddf5d9d5bda4a86da817
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-17 11:53:41 +0100

    Async ollama service

--- src/clients/ollama/service.py
+++ src/clients/ollama/service.py
@@ -2,6 +2,8 @@ import ollama
 import os
 from src.clients.ollama.exceptions import OllamaException
 import logging
+from ollama import ChatResponse
+
 
 logger = logging.getLogger(__name__)
 
@@ -21,3 +23,36 @@ class OllamaService():
                 operation="embed_text",
                 detail=str(e)
             )
+
+class OllamaServiceAsync():
+    def __init__(self):
+        self.client = ollama.AsyncClient(host=os.getenv("OLLAMA_HOST", "http://host.docker.internal:11434"))
+    
+    async def embed_text(self, text:str)->str:
+        try:
+            response = await self.client.embed(model="nomic-embed-text", input=text)
+            return response["embeddings"][0]
+        except ollama.RequestError as e:
+            logger.error(f"Ollama connection Error: {e}")
+            raise OllamaException(
+                operation="async_embed_text",
+                detail=str(e)
+            ) 
+
+    async def create_message(self, texts:list[str], user_input)->str:
+        try:
+            input_string = "\n\n".join(texts)
+            response: ChatResponse = await self.client.chat(model="llama3", messages=[
+                {
+                    "role": "user",
+                    "content": f"Create a response and primarily focus on information from this string: {input_string}"
+                }
+            ])
+            return response['message']['content']  
+
+        except ollama.RequestError as e:
+            logger.error(f"Ollama connection Error: {e}")
+            raise OllamaException(
+                operation="async_create_message",
+                detail=str(e)
+            ) 
