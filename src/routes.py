4bd82cc1fc559268d5709a842638a8902acce813
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-17 11:53:16 +0100

    add chat routes

--- src/routes.py
+++ src/routes.py
@@ -2,13 +2,14 @@ from fastapi import APIRouter
 router = APIRouter()
 from src.modules.document.router import router as document_router
 from src.modules.user.router import router as user_router
+from src.modules.chat.router import router as chat_router
 
 router = APIRouter()
 
 
 router.include_router(user_router, prefix="/user", tags= ["users"])
 router.include_router(document_router, prefix="/document", tags=["document"])
-
+router.include_router(chat_router, prefix="/chat", tags=["chat"])
 
 @router.get("/")
 async def root():
