60de6b44a8a6fba8f81aa301ff37522b4d27d5a0
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-18 13:18:25 +0100

    limit message to 2000 chars and put message to request body

--- src/modules/chat/router.py
+++ src/modules/chat/router.py
@@ -4,12 +4,14 @@ from src.modules.chat.dependencies import get_chat_service
 from src.modules.chat.service import ChatService
 from src.modules.user.dependencies import get_current_user_id
 
+from src.modules.chat.schemas import ChatRequest
+
 router = APIRouter()
 
 @router.post("/",status_code=200, response_model=ChatMessageResponse)
-async def postMessage(input:str, current_user: int = Depends(get_current_user_id), chat_service:ChatService = Depends(get_chat_service)):
-    answer = await chat_service.post_message(user_id=current_user, message=input)
+async def postMessage(request:ChatRequest, current_user: int = Depends(get_current_user_id), chat_service:ChatService = Depends(get_chat_service)):
+    answer = await chat_service.post_message(user_id=current_user, message=request.message)
     return{
         "message" : answer,
-        "reference" : [""]
+        "references" : [""]
     }
--- src/modules/chat/schemas.py
+++ src/modules/chat/schemas.py
@@ -1,7 +1,12 @@
-from pydantic import BaseModel, ConfigDict
-
+from pydantic import BaseModel, ConfigDict, Field
 
 class ChatMessageResponse(BaseModel):
     model_config = ConfigDict(from_attributes=True)
     message : str
-    reference : list[str]
+    references : list[str]
+
+
+
+
+class ChatRequest(BaseModel):
+    message: str = Field(..., min_length=1, max_length=1000)
