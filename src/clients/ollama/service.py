17a62188b396d99403e8e393c5d2a500ea940569
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-30 18:06:44 +0100

    speed improvement

--- src/clients/ollama/service.py
+++ src/clients/ollama/service.py
@@ -18,7 +18,7 @@ class OllamaService():
     def __init__(self):
         self.client = ollama.Client(host=os.getenv("OLLAMA_HOST", "http://host.docker.internal:11434"))
 
-    def embed_text(self, chunks: list[str], batch_size: int = 2, max_retries_per_batch: int = 2):
+    def embed_text(self, chunks: list[str], batch_size: int = 10, max_retries_per_batch: int = 2):
         all_embeddings = []
         skipped_indices = []
         
@@ -30,11 +30,8 @@ class OllamaService():
             batch = chunks[i:i + batch_size]
             batch_num = i // batch_size + 1
             
-            current_keep_alive = -1
-
             if batch_num % reset_after_n_batches == 0:
                 logger.warning(f"Batch {batch_num} is a reset batch. Forcing model reload after this.")
-                current_keep_alive = 0
 
             retry_count = 0
             batch_success = False
@@ -43,11 +40,9 @@ class OllamaService():
                 try:
                     logger.info(f"Processing Batch {batch_num} ({len(batch)} chunks)... [Attempt {retry_count + 1}]")
                     
-                    # dynamically restart ollamas embedding modell based on current_keep_alive var
                     response = self.client.embed(
                         model="nomic-embed-text", 
                         input=batch,
-                        keep_alive=current_keep_alive
                     )
                     
                     # add successfull embeddings to global array 
@@ -57,11 +52,6 @@ class OllamaService():
 
                     del response
                     gc.collect()
-
-                    # when restarting ollama, wait 2 seconds untio next request
-                    if current_keep_alive == 0:
-                        logger.warning("Pausing for 2 seconds to allow Ollama to reset...")
-                        time.sleep(2)
                     
                 except ollama.ResponseError as e:
                     retry_count += 1
