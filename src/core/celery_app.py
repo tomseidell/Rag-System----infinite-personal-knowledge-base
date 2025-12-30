3e5361fd2454efeec11147a3b6f8159705285c81
tomseidell <75682748+tomseidell@users.noreply.github.com>
2025-12-30 17:31:22 +0100

    create qdrant service when initializing worker

--- src/core/celery_app.py
+++ src/core/celery_app.py
@@ -1,5 +1,9 @@
 from celery import Celery
 import os
+from celery.signals import worker_process_init
+from functools import lru_cache
+from src.clients.qdrant.service import QdrantService
+
 
 celery_app = Celery(
     "worker",
@@ -13,10 +17,22 @@ celery_app.conf.update(
     accept_content=["json"],
     task_acks_late=True,
     worker_prefetch_multiplier=1,
-    worker_max_tasks_per_child = 10, # kill the worker after 10 tasks
+    #worker_max_tasks_per_child = 10, # kill the worker after 10 tasks
     task_routes={
         "src.modules.document.workers.*": {"queue": "documents"},
     }
 )
+
+# cache qdrant_service instance in get_qdrant_service function 
+@lru_cache()
+def get_qdrant_service():
+    print("loading Sparse embedding model")
+    qdrant_service = QdrantService()
+    return qdrant_service
+    
+# immediately after initializing celery worker, call get_qdrant_service
+@worker_process_init.connect
+def init_models(**kwargs):
+    get_qdrant_service()
 
 from src.modules.document.workers import process_document  
