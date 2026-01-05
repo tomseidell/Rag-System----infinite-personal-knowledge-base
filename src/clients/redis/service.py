c65075129228cb7869ed40e00b3fc80184f7f98d
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-01-05 17:24:50 +0100

    initialize redis

---
+++ src/clients/redis/service.py
@@ -0,0 +1,15 @@
+import redis.asyncio as redis
+import os  
+
+class RedisService:
+    def __init__(self):
+        self.client = redis.Redis.from_url(url=os.getenv("REDIS_URL","http://localhost:6379"))
+
+    async def get(self, key: str):
+        return await self.client.get(key)
+    
+    async def set(self, key: str, value: str, ttl=None): # ttl = time to live in cache 
+        await self.client.set(key, value, ex=ttl)
+    
+    async def delete(self, key: str):
+        await self.client.delete(key)
