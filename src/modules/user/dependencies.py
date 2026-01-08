515a7758c9ce4a13e04ca04111b5f21caba0b2bb
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-01-08 13:42:17 +0100

    commenting

--- src/modules/user/dependencies.py
+++ src/modules/user/dependencies.py
@@ -3,7 +3,6 @@ from sqlalchemy.ext.asyncio import AsyncSession  # ← AsyncSession!
 from src.database import get_db
 from src.modules.user.repository import UserRepository
 from src.modules.user.service import UserService
-from fastapi import Depends, HTTPException, status
 from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 
 from src.modules.user.utils import decode_access_token
@@ -17,9 +16,11 @@ def get_user_service(repo: UserRepository = Depends(get_user_repository)):
 
 
 
-security = HTTPBearer()
+security = HTTPBearer() # returns scheme and credentials
+
+# dynamic, uncached function
 async def get_current_user_id(credentials:HTTPAuthorizationCredentials = Depends(security)) -> int:
      """Extracts and validates jwt from request and returns user id """
      token = credentials.credentials
-     user_id = decode_access_token(token)
+     user_id = decode_access_token(token) # throws error if token is invalid
      return user_id
