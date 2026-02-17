770e5e5e2763f7c3474837fea0d7a3d34ba3e59d
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-02-17 13:11:51 +0100

    add refresh token logic

--- frontend/src/lib/apiClient.ts
+++ frontend/src/lib/apiClient.ts
@@ -1,17 +1,35 @@
+import { redirect } from "react-router-dom";
 import { AuthService } from "./auth";
 
 const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
 const authService = new AuthService();
+
 async function request<T>(path: string, options?: RequestInit): Promise<T> {
   const res = await fetch(`${baseUrl}/${path}`, {
     ...options,
     headers: {
       "Content-Type": "application/json",
+      // add access token as Bearer to request
       Authorization: `Bearer ${authService.getAccessToken()}`,
       ...options?.headers,
     },
   });
+  // if client is unauthorized => access token outdated
+  if (res.status == 401) {
+    // get new access token with refresh token
+    const refreshed = await authService.tryRefreshToken();
+    // if valid access token was created, retry request
+    if (refreshed) {
+      return request(path, options);
+    }
+    // if no access token could be created, redirect to login page
+    authService.removeTokens();
+    throw redirect("/login");
+  }
+  // catch response errors from api 
   if (!res.ok) throw new Error(res.statusText);
+  
   return res.json();
 }
 
--- frontend/src/lib/auth.ts
+++ frontend/src/lib/auth.ts
@@ -1,8 +1,10 @@
+const baseUrl = import.meta.env.VITE_BACKEND_URL;
+
 export class AuthService {
-  getAccessToken():string | null {
+  getAccessToken(): string | null {
     return localStorage.getItem("accessToken");
   }
-  getRefreshToken():string | null {
+  getRefreshToken(): string | null {
     return localStorage.getItem("refreshToken");
   }
 
@@ -13,8 +15,33 @@ export class AuthService {
     localStorage.setItem("refreshToken", token);
   }
 
-  removeTokens(){
+  removeTokens() {
     localStorage.removeItem("accessToken");
-    localStorage.removeItem("refreshToken")
+    localStorage.removeItem("refreshToken");
+  }
+
+  async tryRefreshToken(): Promise<boolean> {
+    const refreshToken = this.getRefreshToken();
+    if (!refreshToken) {
+      return false;
+    }
+
+    try {
+      const res = await fetch(`${baseUrl}/auth/refresh`, {
+        method: "Post",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ refreshToken }),
+      });
+
+      if(!res.ok){
+        return false
+      }
+
+      const data = await res.json();
+      this.setAccessToken(data.accessToken)
+      return true
+    } catch {
+        return false
+    }
   }
 }
