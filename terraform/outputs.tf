8dc04134298fd07503b98466b950a5122465d6f5
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-04-24 14:04:31 +0200

    add outputs

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -5,3 +5,16 @@ output "api_ecr_url" {
 output "worker_ecr_url" {
   value = aws_ecr_repository.worker.repository_url
 }
+
+output "alb_hostname" {
+  value = "${aws_alb.main.dns_name}:8000"
+}
+
+
+output "db_endpoint" {
+  value = aws_db_instance.postgres.endpoint
+}
+
+output "db_secret_arn" {
+  value = aws_db_instance.postgres.master_user_secret[0].secret_arn
+}
