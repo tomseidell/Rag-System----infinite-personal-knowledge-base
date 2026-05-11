a1799f54f96de1ae5a0192661a6c1063c1b9f0ad
tomseidell <75682748+tomseidell@users.noreply.github.com>
2026-05-11 19:04:37 +0200

    remove custom rate limiting for login and regsiter

--- api/middlewares/rate_limit.py
+++ api/middlewares/rate_limit.py
@@ -2,53 +2,32 @@ from fastapi import Request
 from api.clients.redis.dependency import get_redis_service
 from fastapi.responses import JSONResponse
 
-AUTH_PATHS = {"/user/login", "/user/register"}
-AUTH_LIMIT = 10
-AUTH_TTL = 60 * 60 * 24  # 24 hours
-RATE_LIMIT_BYPASS_IPS = {"127.0.0.1", "::1"}  # localhost — for local e2e tests
+async def rate_limit_middleware(request: Request, call_next): # call next gets automatically handled by fast api
+    redis_service = get_redis_service() # get singleton redis instance 
 
-async def rate_limit_middleware(request: Request, call_next):
-    redis_service = get_redis_service()
-
-    # skip rate limits for urls:
-    if request.url.path in ["/health", "/metrics"]:
-        return await call_next(request)
-
-    if not request.client:
+    if request.url.path in ["/health", "/metrics"]: # allow prometheus and health checks 
+        return await call_next(request) # go to next middleware
+    
+    if not request.client: # decline request without request client 
         return JSONResponse(
             status_code=400,
             content={"detail": "Invalid http request"}
         )
 
-    # skip rate limiting for local development
-    if request.client.host in RATE_LIMIT_BYPASS_IPS:
-        return await call_next(request)
-
     client_ip = request.client.host
-
-    # custom rate limiting for login:
-    if request.url.path in AUTH_PATHS:
-        auth_key = f"ratelimit:auth:{client_ip}"
-        count: int = await redis_service.incr(auth_key)
-        if count == 1:
-            await redis_service.expire(auth_key, AUTH_TTL)
-        if count > AUTH_LIMIT:
-            return JSONResponse(
-                status_code=429,
-                content={"detail": "Too many attempts, try again tomorrow."}
-            )
-
-    # global rate limitter
+ 
     key = f"ratelimit:{client_ip}"
+    
     count: int = await redis_service.incr(key)
     if count == 1:
         await redis_service.expire(key, 60)
-    if count > 100: # 100 requests per minute max
+    
+    if count > 100:
         return JSONResponse(
             status_code=429,
             content={"detail": "Too many requests, try again later."}
         )
-
+    
     return await call_next(request)
 
 
